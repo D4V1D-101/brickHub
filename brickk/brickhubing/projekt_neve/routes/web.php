2e751267584d480d49e5b2ebc99d515cd8f8687c
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-01-14 23:15:04 +0100

    update2
    2

---
+++ brickk/brickhubing/projekt_neve/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Livewire\ShowHome;
+use App\Livewire\ShowService;
+use App\Livewire\ShowServicePage;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', ShowHome::class)->name('home');
+Route::get('/news', ShowServicePage::class)->name('servicesPage');
+Route::get('/new/{id}', ShowService::class)->name('servicePage');
