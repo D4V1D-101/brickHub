24ae88c710679edd711ee83caeb244545505aabb
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-01-16 13:45:45 +0100

    update5
    sadasd

---
+++ brickk/brickhubing/projekt_neve/app/Filament/Resources/ArticleResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Article;
+use App\Models\Category;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ArticleResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ArticleResource\RelationManagers;
+
+class ArticleResource extends Resource
+{
+    protected static ?string $model = Article::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('title')->required()->placeholder('Title'),
+                Select::make('category_id')->label('Category')->options(Category::all()->pluck('name','id')),
+                TextInput::make('author')->required()->placeholder('Author'),
+                FileUpload::make('image')->placeholder('Image'),
+                RichEditor::make('content')->columnSpan(2),
+                Select::make('status')->options([
+                    1 => 'Active',
+                    0 => 'Block'
+                ])->default(1)
+
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->searchable(),
+                TextColumn::make('author'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticles::route('/'),
+            'create' => Pages\CreateArticle::route('/create'),
+            'edit' => Pages\EditArticle::route('/{record}/edit'),
+        ];
+    }
+}
--- brickk/brickhubing/projekt_neve/app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
+++ brickk/brickhubing/projekt_neve/app/Filament/Resources/ArticleResource/Pages/CreateArticle.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace App\Filament\Resources\CategoryResource\Pages;
+namespace App\Filament\Resources\ArticleResource\Pages;
 
-use App\Filament\Resources\CategoryResource;
 use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use App\Filament\Resources\ArticleResource;
 
-class CreateCategory extends CreateRecord
+class CreateArticle extends CreateRecord
 {
-    protected static string $resource = CategoryResource::class;
-
+    protected static string $resource = ArticleResource::class;
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
@@ -19,7 +18,7 @@ class CreateCategory extends CreateRecord
     {
         return Notification::make()
             ->success()
-            ->title('Category Created')
-            ->body('Category has been created successfully.');
+            ->title('Article Created')
+            ->body('Article has been created successfully.');
     }
 }
