3897fbd34649ececc8e57f93af195f4ff2f1f9e9
D4V1D-101 <115972590+D4V1D-101@users.noreply.github.com>
2025-01-19 23:45:47 +0100

    update 6

---
+++ brickk/brickhubing/projekt_neve/resources/views/livewire/blog-detail.blade.php
@@ -0,0 +1,35 @@
+<main>
+    <div class="section">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-10">
+              <div class="mb-5">
+                <h2 class="mb-4" style="line-height:1.5">{{ $article->title }}</h2>
+                <span>{{ Carbon\Carbon::parse($article->created_at)->format('d, M Y') }} <span class="mx-2">/</span> </span>
+                <p class="list-inline-item" class="ml-1">Category :  {{ $article->category_name }} 
+                </p>
+                
+                <p class="list-inline-item" class="ml-1">Author : {{ $article->author }}
+                </p>
+
+
+
+              </div>
+              @if ($article->image !="")
+                  
+             
+              <div class="mb-5 text-center">
+                <div class="post-slider rounded overflow-hidden">
+                  <img loading="lazy" decoding="async" src="{{ asset('storage').'/'.$article->image}}" alt="{{ $article->name }}">
+                  
+                </div>
+              </div>
+              @endif
+              <div class="content">
+                {!! $article->content !!}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</main>
